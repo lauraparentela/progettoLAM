89b88d1305b3df23c105ae04cb6c3fb2c44a9d00
ale <alessia.tasca@studio.unibo.it>
2023-06-01 15:34:34 +0200

    notifiche

--- app/src/main/java/com/example/proglam/MainActivity.java
+++ app/src/main/java/com/example/proglam/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.proglam;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import androidx.core.app.NotificationManagerCompat;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -10,6 +15,7 @@ import android.location.LocationManager;
 import android.media.MediaRecorder;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.CellInfo;
 import android.telephony.CellInfoLte;
@@ -17,12 +23,15 @@ import android.telephony.CellInfoWcdma;
 import android.telephony.CellSignalStrengthLte;
 import android.telephony.CellSignalStrengthWcdma;
 import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -34,20 +43,30 @@ import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 import java.io.IOException;
 
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
-    private static final int PERMISSIONS_REQUEST_CODE = 1;
+
+    private static final int PERMISSIONS_REQUEST_CODE = 1001;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1002;
+
     private MapView mapView;
     private GoogleMap googleMap;
     private LocationManager locationManager;
     private MediaRecorder mediaRecorder;
     private boolean isMonitoring = false;
+    private static final String CHANNEL_ID = "notifica_misurazione";
+    private NotificationManagerCompat notificationManager;
+
+    private boolean areNotificationsEnabled = true; // Aggiungi questa variabile
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManager = NotificationManagerCompat.from(this);
 
-        mapView = new MapView(this);
+
+        mapView = findViewById(R.id.mapView); // Correggi il riferimento all'id corretto
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
 
@@ -67,6 +86,133 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         });
+
+        Button backgroundButton = findViewById(R.id.backgroundButton);
+        backgroundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aggiungi qui la logica per avviare la misurazione in background
+                Toast.makeText(MainActivity.this, "Misurazione in background avviata", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_notifications) {
+            areNotificationsEnabled = !item.isChecked();
+            item.setChecked(areNotificationsEnabled);
+            // Aggiungi qui la logica per abilitare/disabilitare le notifiche in base al valore di areNotificationsEnabled
+            if (areNotificationsEnabled) {
+                Toast.makeText(MainActivity.this, "Notifiche abilitate", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Notifiche disabilitate", Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private boolean isOutOfMeasurementZone(Location location) {
+        // Coordinate centrali dell'esagono
+        double centerLatitude = location.getLatitude();
+        double centerLongitude = location.getLongitude();
+
+        // Calcola la distanza tra la posizione attuale e il centro dell'esagono
+        float[] results = new float[1];
+        Location.distanceBetween(location.getLatitude(), location.getLongitude(), centerLatitude, centerLongitude, results);
+        double distanceToCenter = results[0];
+
+        // Calcola la lunghezza del lato dell'esagono
+        double sideLength = 100; // in metri
+
+        // Calcola il raggio dell'esagono (distanza dal centro all'angolo dell'esagono)
+        double hexagonRadius = sideLength / Math.sqrt(3);
+
+        // Verifica se la distanza dal centro supera il raggio dell'esagono
+        return distanceToCenter > hexagonRadius;
+    }
+    private void sendNotification(String title, String message, Context context) {
+        // Creazione dell'intent per aprire l'attività principale quando si fa clic sulla notifica
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Creazione del canale di notifica (solo per Android Oreo e versioni successive)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "Measurement Notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        // Creazione della notifica
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        // Richiesta dei permessi di accesso alla posizione
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    private void sendNotificationIfOutOfMeasurementZone() {
+        try {
+            locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (isOutOfMeasurementZone(location)) {
+                        sendNotification("Area senza misurazioni", "Non sono state fatte misurazioni nell'area circostante.", MainActivity.this);
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                    // Aggiungi qui la logica per il cambio di stato del provider di posizione
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                    // Aggiungi qui la logica per quando il provider di posizione viene abilitato
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                    // Aggiungi qui la logica per quando il provider di posizione viene disabilitato
+                }
+            }, null);
+        } catch (SecurityException e) {
+            // Permesso di accesso alla posizione non disponibile, gestisci di conseguenza
+            Toast.makeText(MainActivity.this, "Permesso di accesso alla posizione negato", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                // Permesso di accesso alla posizione ottenuto, invia la notifica
+                sendNotificationIfOutOfMeasurementZone();
+            } else {
+                Toast.makeText(this, "Permessi necessari non concessi.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -262,7 +408,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 // Implementa altri metodi di callback per la gestione delle autorizzazioni e gli eventi del GPS
 
-    @Override
+    /*@Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSIONS_REQUEST_CODE) {
@@ -273,7 +419,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 Toast.makeText(this, "Permessi necessari non concessi.", Toast.LENGTH_SHORT).show();
             }
         }
-    }
+    }*/
 
 // Implementa altri metodi di callback per la gestione degli eventi del GPS
 
